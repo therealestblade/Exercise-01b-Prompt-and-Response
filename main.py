40cf08e9f7af48d35b5a9dbef8bbab35d5b35427
therealestblade <89621761+therealestblade@users.noreply.github.com>
2021-08-31 21:47:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+
+print("You are the " + passage_name)
+print(passage_text)
+response = input("What would you like to do?")
+if response == " Go North":
+    print("Good job!")
